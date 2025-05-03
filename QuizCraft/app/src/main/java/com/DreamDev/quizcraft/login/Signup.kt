faf01b43b4db6b57e05f05a05c634a296a281906
Siddique Ahmad Ryan <ryan.ahmad2923@gmail.com>
2025-05-03 11:40:17 +0500

    add login and sign up screens

--- QuizCraft/app/src/main/java/com/DreamDev/quizcraft/login/Signup.kt
+++ QuizCraft/app/src/main/java/com/DreamDev/quizcraft/login/Signup.kt
@@ -1,31 +1,21 @@
-package com.DreamDev.quizcraft
+package com.DreamDev.quizcraft.login
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-import android.os.Handler
-import android.os.Looper
-import com.DreamDev.quizcraft.login.Login
+import com.DreamDev.quizcraft.R
 
-class MainActivity : AppCompatActivity() {
+class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signup)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        // Delay for 5 seconds and navigate to Login screen
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000) // 5000 milliseconds = 5 seconds
     }
 }
