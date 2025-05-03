ab32d6737d85655cb033fc6e743ebf19519e3a2a
Siddique Ahmad Ryan <ryan.ahmad2923@gmail.com>
2025-05-04 02:23:31 +0500

    update fragments

--- QuizCraft/app/src/main/java/com/DreamDev/quizcraft/login/Login.kt
+++ QuizCraft/app/src/main/java/com/DreamDev/quizcraft/login/Login.kt
@@ -1,11 +1,16 @@
 package com.DreamDev.quizcraft.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import com.DreamDev.quizcraft.HomeActivity
 import com.DreamDev.quizcraft.R
+import com.DreamDev.quizcraft.ui.home.HomeFragment
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +22,19 @@ class Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val signupbtn = findViewById<TextView>(R.id.signup_btn)
+        signupbtn.setOnClickListener {
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val loginbtn = findViewById<TextView>(R.id.login_btn)
+        loginbtn.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
--- QuizCraft/app/src/main/java/com/DreamDev/quizcraft/login/Signup.kt
+++ QuizCraft/app/src/main/java/com/DreamDev/quizcraft/login/Signup.kt
@@ -1,10 +1,13 @@
 package com.DreamDev.quizcraft.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.DreamDev.quizcraft.HomeActivity
 import com.DreamDev.quizcraft.R
 
 class Signup : AppCompatActivity() {
@@ -16,6 +19,18 @@ class Signup : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val loginbtn = findViewById<TextView>(R.id.loginRedirect)
+        loginbtn.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+        val signupbtn = findViewById<TextView>(R.id.signup_btn)
+        signupbtn.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
