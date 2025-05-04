f3f2d0a1bd6b5b690678c56d82d406351c1b04d3
Siddique Ahmad Ryan <ryan.ahmad2923@gmail.com>
2025-05-04 19:51:33 +0500

    update home layout

--- QuizCraft/app/src/main/java/com/DreamDev/quizcraft/ui/classroom/Classroom.kt
+++ QuizCraft/app/src/main/java/com/DreamDev/quizcraft/ui/classroom/Classroom.kt
@@ -1,10 +1,11 @@
-package com.DreamDev.quizcraft
+package com.DreamDev.quizcraft.ui.classroom
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.DreamDev.quizcraft.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Xps.newInstance] factory method to
+ * Use the [Classroom.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Xps : Fragment() {
+class Classroom : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class Xps : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_xps, container, false)
+        return inflater.inflate(R.layout.fragment_classroom, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class Xps : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Xps.
+         * @return A new instance of fragment Classroom.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Xps().apply {
+            Classroom().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
